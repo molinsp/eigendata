b523e0e30e79aeabc04b9d6469f39605ef6d5802
Pere Molins Ayuso <molinsp@gmail.com>
2020-10-25 00:44:19 +0300

    Lay ground for data visualizer extension

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import {
 } from '@jupyterlab/application';
 
 import { MainAreaWidget, WidgetTracker } from '@jupyterlab/apputils';
-import { FormWidget, Backend } from './widget';
+import { FormWidget, Backend } from './formulabar';
+import { DataVisualizer } from './datavisualizer';
 import { reactIcon } from '@jupyterlab/ui-components';
 
 import {
@@ -17,10 +18,11 @@ import { IMainMenu } from '@jupyterlab/mainmenu';
 import { Menu } from '@lumino/widgets';
 
 /**
- * The command IDs used by the react-widget plugin.
+ * The command IDs used by the react-formulawidget plugin.
  */
 namespace CommandIDs {
   export const create = 'ed:open';
+  export const dataviz = 'ed:opendataviz';
 }
 
 /**
@@ -33,51 +35,92 @@ const extension: JupyterFrontEndPlugin<void> = {
   activate: (app: JupyterFrontEnd, notebook_tracker: INotebookTracker, mainMenu: IMainMenu, restorer: ILayoutRestorer) => {
     console.log('JupyterLab Eigendata is activated!');
     
-    let widget : MainAreaWidget<FormWidget>;
-    console.log('Main Area Widget created: ', widget);
+    let formulawidget : MainAreaWidget<FormWidget>;
+    let datavizwidget : MainAreaWidget<DataVisualizer>;
 
     const { commands } = app;
     const command = CommandIDs.create;
+    const datavizcommand = CommandIDs.dataviz;
+
+    // Create class that manages the backend behavior
+    const backend = new Backend(notebook_tracker);
 
     commands.addCommand(command, {
       caption: 'Create a new React Widget',
       label: 'Magic Formula Bar',
       icon: args => (args['isPalette'] ? null : reactIcon),
       execute: () => {
-        if (!widget || widget.isDisposed) {
-          // Create class that manages the backend behavior
-          const backend = new Backend(notebook_tracker);
+        if (!formulawidget || formulawidget.isDisposed) {
           // Create form component and pass backend behavior
           const content = new FormWidget(backend);
-          // Create widget
-          widget = new MainAreaWidget<FormWidget>({ content });
-          widget.title.label = 'Magic Formula Bar';
-          widget.title.closable = true;
-          //app.shell.add(widget, 'main');
+          // Create formulawidget
+          formulawidget = new MainAreaWidget<FormWidget>({ content });
+          formulawidget.title.label = 'Magic Formula Bar';
+          formulawidget.title.closable = true;
+          //app.shell.add(formulawidget, 'main');
         }
-        if (!tracker.has(widget)) {
-        // Track the state of the widget for later restoration
-        tracker.add(widget);
+        if (!tracker.has(formulawidget)) {
+        // Track the state of the formulawidget for later restoration
+        tracker.add(formulawidget);
         }
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.add(widget, 'main');
+        if (!formulawidget.isAttached) {
+          // Attach the formulawidget to the main work area if it's not there
+          app.shell.add(formulawidget, 'main');
         }
-        widget.content.update();
+        formulawidget.content.update();
 
-        // Activate the widget
-        app.shell.activateById(widget.id);
+        // Activate the formulawidget
+        app.shell.activateById(formulawidget.id);
         }
     });
 
-      // Track and restore the widget state
-      let tracker = new WidgetTracker<MainAreaWidget<FormWidget>>({
-        namespace: 'ed'
-      });
-      restorer.restore(tracker, {
-        command,
-        name: () => 'ed'
-      });
+    commands.addCommand(datavizcommand, {
+      caption: 'Create a new Data Data Visualizer',
+      label: 'Data Visualizer',
+      icon: args => (args['isPalette'] ? null : reactIcon),
+      execute: () => {
+        if (!datavizwidget || datavizwidget.isDisposed) {
+          // Create form component and pass backend behavior
+          const content = new DataVisualizer();
+          // Create datavizwidget
+          datavizwidget = new MainAreaWidget<DataVisualizer>({ content });
+          datavizwidget.title.label = 'Data Visualizer';
+          datavizwidget.title.closable = true;
+          //app.shell.add(datavizwidget, 'main');
+        }
+        if (!tracker.has(datavizwidget)) {
+        // Track the state of the datavizwidget for later restoration
+          viztracker.add(datavizwidget);
+        }
+        if (!datavizwidget.isAttached) {
+          // Attach the datavizwidget to the main work area if it's not there
+          app.shell.add(datavizwidget, 'main');
+        }
+        datavizwidget.content.update();
+
+        // Activate the datavizwidget
+        app.shell.activateById(datavizwidget.id);
+        }
+    });
+
+    // Track and restore the formulawidget state
+    let tracker = new WidgetTracker<MainAreaWidget<FormWidget>>({
+      namespace: 'ed'
+    });
+    restorer.restore(tracker, {
+      command,
+      name: () => 'ed'
+    });
+
+    // Track and restore the formulawidget state
+    let viztracker = new WidgetTracker<MainAreaWidget<DataVisualizer>>({
+      namespace: 'ed'
+    });
+    restorer.restore(viztracker, {
+      command: datavizcommand,
+      name: () => 'ed'
+    });
+
 
     // Create a menu
     const tutorialMenu: Menu = new Menu({ commands });
@@ -86,6 +129,7 @@ const extension: JupyterFrontEndPlugin<void> = {
 
     // Add the command to the menu
     tutorialMenu.addItem({ command, args: { origin: 'from the menu' } });
+    tutorialMenu.addItem({ command: datavizcommand, args: { origin: 'from the menu' } });
 
   }
 };
