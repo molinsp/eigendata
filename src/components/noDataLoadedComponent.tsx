aaab6bea460a761aeae2695012d04e797dbb9e5c
Islam Rustamov <islam.rustam0v28et@gmail.com>
2021-02-12 12:28:09 +0300

    [Datavizualizer] Add empty state component

---
+++ src/components/noDataLoadedComponent.tsx
@@ -0,0 +1,22 @@
+// [COMPONENT] Empty state component
+//   -> Description: is shown if there are no dataframes loaded
+//   -> Used in: Datavizualizer
+
+import React from 'react';
+import { noDataMagnifier } from '../assets/svgs';
+
+export const EmptyStateComponent = ({ onLinkClick }: Props): JSX.Element => {
+  return (
+    <div className="empty-state__container">
+      <p className="empty-state__title">No data loaded</p>
+      <a className="empty-state__link" onClick={(): void => onLinkClick()}>
+        Click here to load data with the magic formula bar
+      </a>
+      <div>{noDataMagnifier}</div>
+    </div>
+  );
+};
+
+type Props = {
+  onLinkClick(): void;
+};
