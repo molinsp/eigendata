8400fbb788c09965ed77dca2a1d3a0da736af95d
Pere Molins Ayuso <molinsp@gmail.com>
2021-02-12 12:24:50 +0200

    Change text empty state

--- src/components/noDataLoadedComponent.tsx
+++ src/components/noDataLoadedComponent.tsx
@@ -9,9 +9,9 @@ export const EmptyStateComponent = ({ onLinkClick }: Props): JSX.Element => {
   return (
     <div className="empty-state__container">
       <p className="empty-state__title">No data loaded</p>
-      <a className="empty-state__link" onClick={(): void => onLinkClick()}>
-        Click here to load data with the magic formula bar
-      </a>
+      <p className="empty-state__link">
+        Use the Magic Formula Bar to load data
+      </p>
       <div>{noDataMagnifier}</div>
     </div>
   );
