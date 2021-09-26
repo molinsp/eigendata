e58180425f24ed946ff48750c8b2aba92a5b1fc5
Pere Molins Ayuso <molinsp@gmail.com>
2021-09-26 21:38:52 +0300

    [Querybuilder] Show errors

--- src/components/querybuilder.tsx
+++ src/components/querybuilder.tsx
@@ -31,7 +31,8 @@ export default class QueryBuilder extends Component<
       config: config,
       logic: props.backend,
       queryType: 'query',
-      newTableName: ''
+      newTableName: '',
+      error: null
     };
   }
 
@@ -62,6 +63,16 @@ export default class QueryBuilder extends Component<
 
   render = (): JSX.Element => (
     <form id="query-form">
+      {this.state.error && (
+      <div className="panel panel-danger errors">
+        <div className="panel-heading">
+          <h3 className="panel-title">Errors</h3>
+        </div>
+        <ul className="list-group">
+          <li className="list-group-item text-danger">{this.state.error.message}</li>
+        </ul>
+      </div>
+      )}
       <div className="form-group">
         <fieldset>
           <legend>Filter/Query dataframe</legend>
@@ -130,116 +141,134 @@ export default class QueryBuilder extends Component<
       2
     );
     console.log('Querybuilder: Original SQL query', sqlQuery);
-    
-    sqlQuery = sqlQuery
-      .replace(/ IS EMPTY/g, '.isnull()')
-      .replace(/ IS NOT EMPTY/g, '.notnull()')
-      .replace(/AND/g, 'and')
-      .replace(/OR/g, 'or')
-      .replace(/true/g, 'True')
-      .replace(/false/g, 'False') 
-      // Handle the case of any in ['a','b'] instead of IN ('a', 'b')  
-      .replace(/NOT IN\s\((.*?)\)/g, 'not in [$1]') 
-      // Not and not in in python
-      .replace(/IN\s\((.*?)\)/g, 'in [$1]')
-      .replace(/NOT/g, '~')
-      // Replace comparison to remove spaces (convention)
-      .replace(/ >= /g, '>=')
-      .replace(/ <= /g, '<=')
-      .replace(/ > /g, '>')
-      .replace(/ < /g, '<')
-      // Use python characters for equal to and distinct to
-      .replace(/ <> /g, '!=')
-      .replace(/ = /g, '==');
 
-    const colNames = Object.entries(this.state.config.fields).map(
-      ([k, v]) => v.label
-    );
-    //console.log('Querybuilder: Config fields type', typeof colNames);
-    //console.log('Querybuilder: Config fields', colNames);
-    const colNamesSorted = colNames.sort((a, b) => b.length - a.length);
-    //console.log('Querybuilder: Config fields sorted', colNamesSorted);
+    if(sqlQuery){    
+      sqlQuery = sqlQuery
+        .replace(/ IS EMPTY/g, '.isnull()')
+        .replace(/ IS NOT EMPTY/g, '.notnull()')
+        .replace(/AND/g, 'and')
+        .replace(/OR/g, 'or')
+        .replace(/true/g, 'True')
+        .replace(/false/g, 'False') 
+        // Handle the case of any in ['a','b'] instead of IN ('a', 'b')  
+        .replace(/NOT IN\s\((.*?)\)/g, 'not in [$1]') 
+        // Not and not in in python
+        .replace(/IN\s\((.*?)\)/g, 'in [$1]')
+        .replace(/NOT/g, '~')
+        // Replace comparison to remove spaces (convention)
+        .replace(/ >= /g, '>=')
+        .replace(/ <= /g, '<=')
+        .replace(/ > /g, '>')
+        .replace(/ < /g, '<')
+        // Use python characters for equal to and distinct to
+        .replace(/ <> /g, '!=')
+        .replace(/ = /g, '==');
 
-    console.log('Querybuilder: Processed sql query', sqlQuery);
-    for (const i of colNamesSorted) {
-      const colName = i;
-      //console.log('Col name', colName);
-      // Check if there is any blank space in the column names
-      if (/\s/.test(colName) && sqlQuery.includes(colName)) {
-        // It has any kind of whitespace
-        // Replace with backticks to ensure pandas does not generate an error;
-        // To avoid replacing columns that are substrings of other columns with backticks
-        // E.g. "col on", "cole one two" -> "`col one` two"
-        // To avoid this, we replace only strins that do not end with space
-        // since by convention we don't use spaces for comparison operators
-        // - Numercial operators. e.g. "col a>5"
-        // - String and null operators, e.g. "col a.isnull"
-        // EXCEPT for the cases of "in [" / "not in ["
-        const colsWithBlanks = "(" + colName + ")(?! (?!in \\[|not in \\[))";
-        const colsAlsoCategories = "'`" + colName + "`'";
-        const reColsWithBlanks = new RegExp(colsWithBlanks, 'g');
-        const reColsAlsoCategories = new RegExp(colsAlsoCategories,'g');
-        console.log('re',colsWithBlanks);
-        sqlQuery = sqlQuery
-          .replace(reColsWithBlanks,"`" + colName + "`")
-          .replace(reColsAlsoCategories,"'" + colName + "'");
+      const colNames = Object.entries(this.state.config.fields).map(
+        ([k, v]) => v.label
+      );
+      //console.log('Querybuilder: Config fields type', typeof colNames);
+      //console.log('Querybuilder: Config fields', colNames);
+      const colNamesSorted = colNames.sort((a, b) => b.length - a.length);
+      //console.log('Querybuilder: Config fields sorted', colNamesSorted);
 
+      console.log('Querybuilder: Processed sql query', sqlQuery);
+      for (const i of colNamesSorted) {
+        const colName = i;
+        //console.log('Col name', colName);
+        // Check if there is any blank space in the column names
+        if (/\s/.test(colName) && sqlQuery.includes(colName)) {
+          // It has any kind of whitespace
+          // Replace with backticks to ensure pandas does not generate an error;
+          // To avoid replacing columns that are substrings of other columns with backticks
+          // E.g. "col on", "cole one two" -> "`col one` two"
+          // To avoid this, we replace only strins that do not end with space
+          // since by convention we don't use spaces for comparison operators
+          // - Numercial operators. e.g. "col a>5"
+          // - String and null operators, e.g. "col a.isnull"
+          // EXCEPT for the cases of "in [" / "not in ["
+          const colsWithBlanks = "(" + colName + ")(?! (?!in \\[|not in \\[))";
+          const colsAlsoCategories = "'`" + colName + "`'";
+          const reColsWithBlanks = new RegExp(colsWithBlanks, 'g');
+          const reColsAlsoCategories = new RegExp(colsAlsoCategories,'g');
+          console.log('re',colsWithBlanks);
+          sqlQuery = sqlQuery
+            .replace(reColsWithBlanks,"`" + colName + "`")
+            .replace(reColsAlsoCategories,"'" + colName + "'");
+
+        }
       }
-    }
 
-    let returnType = '';
-    if (this.state.queryType.localeCompare('query') === 0) {
-      returnType = 'dataframe';
-    } else {
-      // eval case
-      returnType = 'series';
-    }
-
-    // If no variable defined, use this dataframe selection
-    let variable = '';
-    if (returnType.localeCompare('dataframe') === 0) {
-      if (this.state.newTableName === '') {
-        variable = this.props.dataframeSelection;
+      let returnType = '';
+      if (this.state.queryType.localeCompare('query') === 0) {
+        returnType = 'dataframe';
       } else {
-        variable = this.state.newTableName.replace(/ /g, '_');
+        // eval case
+        returnType = 'series';
       }
-    } else if (returnType.localeCompare('series') === 0) {
-      if (this.state.newTableName === '') {
-        variable = this.props.dataframeSelection + '["indicator"]';
+
+      // If no variable defined, use this dataframe selection
+      let variable = '';
+      if (returnType.localeCompare('dataframe') === 0) {
+        if (this.state.newTableName === '') {
+          variable = this.props.dataframeSelection;
+        } else {
+          variable = this.state.newTableName.replace(/ /g, '_');
+        }
+      } else if (returnType.localeCompare('series') === 0) {
+        if (this.state.newTableName === '') {
+          variable = this.props.dataframeSelection + '["indicator"]';
+        } else {
+          variable =
+            this.props.dataframeSelection +
+            '["' +
+            this.state.newTableName.replace(/ /g, '_') +
+            '"]';
+        }
+      }
+
+      //  Compose formula: variable = dataframe . query/eval (query)
+      let formula = variable + ' = ' + this.props.dataframeSelection;
+      formula = formula + '.' + this.state.queryType;
+
+      // Add the copy command to make sure pandas knows we are creating a copy
+      if (returnType.localeCompare('dataframe') === 0) {
+        formula = formula + '(' + sqlQuery + ', engine="python"' + ').copy()';
       } else {
-        variable =
-          this.props.dataframeSelection +
-          '["' +
-          this.state.newTableName.replace(/ /g, '_') +
-          '"]';
+        formula = formula + '(' + sqlQuery + ', engine="python"' + ')';
       }
-    }
 
-    //  Compose formula: variable = dataframe . query/eval (query)
-    let formula = variable + ' = ' + this.props.dataframeSelection;
-    formula = formula + '.' + this.state.queryType;
+      console.log(
+        'Querybuilder: Datatable selection',
+        this.props.dataframeSelection
+      );
+      console.log('Querybuilder: Formula', formula);
 
-    // Add the copy command to make sure pandas knows we are creating a copy
-    if (returnType.localeCompare('dataframe') === 0) {
-      formula = formula + '(' + sqlQuery + ', engine="python"' + ').copy()';
-    } else {
-      formula = formula + '(' + sqlQuery + ', engine="python"' + ')';
-    }
+      if (this.state.logic.production && this.state.logic.shareProductData) {
+        amplitude.getInstance().logEvent('Querybuilder: submit transformation', {
+          function: 'query',
+          generatedCode: formula
+        });
+      }
 
-    console.log(
-      'Querybuilder: Datatable selection',
-      this.props.dataframeSelection
-    );
-    console.log('Querybuilder: Formula', formula);
-
-    if (this.state.logic.production && this.state.logic.shareProductData) {
-      amplitude.getInstance().logEvent('Querybuilder: submit transformation', {
-        function: 'query',
-        generatedCode: formula
+      try{
+        await this.state.logic.writeToNotebookAndExecute(formula, returnType);
+        this.setState(state => ({
+          ...state, 
+          error: null}));
+      }catch(error){
+        this.setState(state => ({
+          ...state, 
+          error: error}));
+      }
+    }else{
+      var error = new ErrorEvent('oh nose', {
+          message : 'Invalid query: The submited query is not valid, please check your inputs.',
       });
+      this.setState(state => ({
+          ...state, 
+          error: error}));
     }
-
-    await this.state.logic.writeToNotebookAndExecute(formula, returnType);
   };
 
   /*----------------------------------------------------------------------------------------------------
@@ -268,4 +297,5 @@ interface IQueryBuilderState {
   logic: Backend;
   queryType: string;
   newTableName: string;
+  error: ErrorEvent
 }
